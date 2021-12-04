ad1ca459714d3105aa898f71197566fa96b925fe
Samson N <samnzumg@gmail.com>
2021-12-04 18:09:42 +0100

    My task App with angular v1

---
+++ taskApp/src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string = 'TaskApp Tracker';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+    console.log('toggle')
+  }
+}
