41ad100794b25bf55b1ec7365d5bb1589292c807
Samson N <samnzumg@gmail.com>
2021-12-04 21:02:02 +0100

    My task App with angular v2

--- taskApp/src/app/components/header/header.component.ts
+++ taskApp/src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title: string = 'TaskApp Tracker';
+  title: string = 'Task Tracker App';
 
   constructor() { }
 
